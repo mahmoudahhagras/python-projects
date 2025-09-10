190efe5108dea6adae55c69501cc2a915b466da5
Ryo <mahmoudahhagras@gmail.com>
2025-09-10 16:41:59 +0300

    finshed the hangman game

--- Part 24 - Hangman Game/main.py
+++ Part 24 - Hangman Game/main.py
@@ -1,20 +1,45 @@
-# a hangman game in progress
+from hangman_words import word_list
+from hangman_art import logo, stages
 import random
-word_list = ["aardvark", "baboon", "camel"]
+print(logo)
 chosen_word = random.choice(word_list)
-print("Rules:\nyour current hp is \nevery wrong answer will take your hp.")
+place_holder = []
+letters = len(chosen_word)
+while len(place_holder) < letters:
+    place_holder.append("_")
+print("Rules:\nyour current HP is 3\nevery wrong answer will take off 1 hp.")
 correct = 0
-hp = 1
-while hp > 0:
-    guess = input("take a guess?")
-    for letter in chosen_word:
-        if letter == guess.lower():
-            
+hp = 3
+no = 0
+display = ""
+while hp > 0 and display != chosen_word:
+    guess = input("take a guess?\n")
+    for letter in chosen_word :
+        if letter == guess.lower() :
             correct = 1
-    if correct > 0:
-        print("You Win")
+            place_holder[no] = letter
+            no+=1
+        else:
+            no+=1
+    if correct > 0 :
+        print("Correct")
         correct = 0
+        no = 0
+        display = "".join(place_holder)
+        print(display)
+        print("Your Current HP is " + str(hp))
+        print(stages[hp])
     else:
-        hp-= 1
+        hp -= 1
+        no = 0
+        print("Incorrect\n-1 HP")
+        if hp == 0:
+            print("Your Current HP is 0\nYou Lose")
+            print(stages[0])
+            exit()
+        elif hp != 0:
+            print("Your Current HP is " + str(hp))
+            print(stages[hp])
 else:
-    print("You lose")
+    print("You Win")
+    print("Your Current HP is " + str(hp) + "\nThe word was " + display.capitalize())
