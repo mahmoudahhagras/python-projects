30c14b0a383d209a75f0b4405ef09982d194c3b0
Ryo <mahmoudahhagras@gmail.com>
2025-09-19 23:16:36 +0300

    refactor: enhance Black Jack game logic and improve replay functionality

--- Part 033 - Capstone Project 1 - BlackJack Game/main.py
+++ Part 033 - Capstone Project 1 - BlackJack Game/main.py
@@ -1,26 +1,38 @@
+from art import logo
+print(logo)
 import random
 print("welcome to the Black Jack game!!")
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 user_cards = []
 console_cards = []
 con = True
+repeat = "yes"
 move_user = ""
-def calcu(move_user , cards, user_cards, console_cards, con):
+def calcu(move_user , cards, user_cards, console_cards, con, repeat):
     console_score = sum(console_cards)
     user_score = sum(user_cards)
     if user_score > 21:
         if 11 in user_cards and user_score > 21:
-            user_cards[user_cards.index(11)] = 1
-            user_score = sum(user_cards)
+            while 11 in user_cards and user_score > 21:
+                user_cards[user_cards.index(11)] = 1
+                user_score = sum(user_cards)
         else:
             print(f"User Loses Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
+            repeat = input("Do you want to try again?")
+            return repeat
     elif user_score == 21:
         print(f"User Wins Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
+        repeat = input("Do you want to try again?")
+        return repeat
     elif console_score == 21:
         print(f"Console Wins Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
+        repeat = input("Do you want to try again?")
+        return repeat
     elif console_score > 21:
         print(f"User Wins Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
-def deal_cards(move_user , cards, user_cards, console_cards, con):
+        repeat = input("Do you want to try again?")
+        return repeat
+def deal_cards(move_user , cards, user_cards, console_cards, con, repeat):
     user_cards.append(random.choice(cards))
     user_cards.append(random.choice(cards))
     user_score = sum(user_cards)
@@ -35,19 +47,38 @@ def deal_cards(move_user , cards, user_cards, console_cards, con):
                 user_cards.append(random.choice(cards))
                 print(f"Your cards are {user_cards}")
                 user_score = sum(user_cards)
-            elif move_user.lower() == "stand":
-                print(f"Your cards are {user_cards}")
-                user_score = sum(user_cards)
-                calcu(move_user , cards, user_cards, console_cards, con)
-                break
+                repeat = calcu(move_user , cards, user_cards, console_cards, con, repeat)
+                if sum(user_cards) >= 21:
+                    break
             if console_score >= 18:
                 print(f"the console has chosen to stand")
-                console_score = sum(console_cards)
             elif console_score < 18:
-                console_cards.append( random.choice(cards))
+                console_cards.append(random.choice(cards))
                 console_score = sum(console_cards)
-                print(f"the console has made it's move")
+                repeat = calcu(move_user , cards, user_cards, console_cards, con, repeat)
+                if console_score >= 21:
+                    break
+            if move_user.lower() == "stand":
+                user_score = sum(user_cards)
+                repeat = calcu(move_user , cards, user_cards, console_cards, con, repeat)
+                if user_score > console_score and user_score <= 21:
+                    print(f"User Wins Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
+                    repeat = input("Do you want to try again?")
+                    return repeat
+                else:  
+                    print(f"Console Wins Your score is {sum(user_cards)}\nand Console score = {sum(console_cards)}")
+                    repeat = input("Do you want to try again?")
+                    return repeat
 start = input("Type 'start' to start\n")
-if start == "start":
-    deal_cards(move_user , cards, user_cards, console_cards, con)
-    calcu(move_user , cards, user_cards, console_cards, con)
+while start == "start" and repeat == "yes":
+    console_score = 0
+    user_score = 0
+    user_cards = []
+    console_cards = []
+    con = True
+    repeat = "yes"
+    move_user = ""
+    repeat = deal_cards(move_user , cards, user_cards, console_cards, con, repeat)
+else:
+    print("Thank you for playing until we meet again.")
+    exit()
